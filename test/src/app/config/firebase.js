9bbf04688bc5fd06cc54ff066e06de2af5f0a1bd
Logan Yee <133840979+logan-yee@users.noreply.github.com>
2024-11-28 12:57:35 -0500

    ...

--- test/src/app/config/firebase.js
+++ test/src/app/config/firebase.js
@@ -4,13 +4,13 @@ import { GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOwpsvC9TjUyd2b10rAbZlA9cOh4TRBAk",
-  authDomain: "naan-stop-wok-a9fbd.firebaseapp.com",
-  projectId: "naan-stop-wok-a9fbd",
-  storageBucket: "naan-stop-wok-a9fbd.firebasestorage.app",
-  messagingSenderId: "976206608524",
-  appId: "1:976206608524:web:5eeb13a518d935557023a9",
-  measurementId: "G-44N5CHSFX1"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
